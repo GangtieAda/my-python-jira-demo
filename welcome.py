1f5ea2cec38fc57dddc5a0a707199e049ab56b30
Charlotte Deng <83746828+GangtieAda@users.noreply.github.com>
2025-10-29 12:52:16 -0400

    Update greeting message in welcome.py

--- welcome.py
+++ welcome.py
@@ -1,6 +1,6 @@
 # welcome.py
 def greet(name):
-    return f"Hello, {name}! Welcome to our project management assignemnt #4."
+    return f"Hello, {name}! Welcome to our project management assignemnt #4 - Demo."
 
 if __name__ == "__main__":
     print(greet("Developer"))
