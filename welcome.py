790258d9a43bd20781f947ba08769cf6f7c043fa
Charlotte Deng <83746828+GangtieAda@users.noreply.github.com>
2025-10-29 13:39:23 -0400

    Fix greeting message in welcome.py

--- welcome.py
+++ welcome.py
@@ -1,6 +1,6 @@
 # welcome.py
 def greet(name):
-    return f"Hello, {name}! Welcome to our project management assignemnt #4 - Demo."
+    return f"Hello, {name}! Welcome to our project management assignemnt #4."
 
 if __name__ == "__main__":
     print(greet("Developer"))
